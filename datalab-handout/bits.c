465172538ff9eb8724515c65fab65ac08f6d01d2
OmidErnesto <ochahuaris@unsa.edu.pe>
2022-07-03 18:05:41 -0500

    Ejercicio 9 resuleto

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -245,7 +245,8 @@ int isLessOrEqual(int x, int y) {
  *   Rating: 4 
  */
 int logicalNeg(int x) {
-  return 2;
+  x = (x | (~x+1)) >> 31; //Signo 
+  return ~x & 1;
 }
 /* howManyBits - return the minimum number of bits required to represent x in
  *             two's complement
